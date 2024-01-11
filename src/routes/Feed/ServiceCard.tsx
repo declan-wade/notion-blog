99f600747433e588f743109a0e2e902dc80152b1
declan-wade <17171018+declan-wade@users.noreply.github.com>
2024-01-11 09:58:15 +0800

    Update ServiceCard.tsx

--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -9,7 +9,7 @@ const ServiceCard: React.FC = () => {
   return (
     <>
       <StyledTitle>
-        <Emoji>🌟</Emoji> Service
+        <Emoji>🚀</Emoji> Projects
       </StyledTitle>
       <StyledWrapper>
         {CONFIG.projects.map((project, idx) => (
